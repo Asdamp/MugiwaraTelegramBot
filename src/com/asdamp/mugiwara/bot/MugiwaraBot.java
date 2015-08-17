678d5d8708d32d3b2b1797e52ec80a8bb88eb937
Antonio Altieri <antonio.danilo92@gmail.com>
2015-08-17 15:38:42 +0200

    Update webhook

--- src/com/asdamp/mugiwara/bot/MugiwaraBot.java
+++ src/com/asdamp/mugiwara/bot/MugiwaraBot.java
@@ -4,17 +4,20 @@ import co.vandenham.telegram.botapi.CommandHandler;
 import co.vandenham.telegram.botapi.DefaultHandler;
 import co.vandenham.telegram.botapi.MessageHandler;
 import co.vandenham.telegram.botapi.TelegramBot;
+import co.vandenham.telegram.botapi.requests.ApiResponse;
 import co.vandenham.telegram.botapi.requests.OptionalArgs;
 import co.vandenham.telegram.botapi.types.Message;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class MugiwaraBot extends TelegramBot {
 	private final String help="Questo e' un bot di utilita' per il clan Mugiwara. Ulteriori funzioni verranno aggiunte in futuro Lista comandi\n\n /regolamento - Posta il regolamento del clan"; 
-    
+    private String regolamento=null;
 	private static final Logger log = Logger.getLogger(MugiwaraBot.class.getName());
 
     public MugiwaraBot(boolean async) {
@@ -33,26 +36,31 @@ public class MugiwaraBot extends TelegramBot {
     @CommandHandler("regolamento")
     public void handleRegolamento(Message message){
     	Scanner sc;
-    	try {
-			sc=new Scanner(new File("regolamento.txt"));
-			String regolamento="";
-	    	while(sc.hasNextLine()){
-	    		regolamento=regolamento+sc.nextLine()+"\n";
-	    	}
-	    	
-	        sendMessage(message.getChat().getId(), regolamento, new OptionalArgs().disableWebPagePreview());
-		} catch (FileNotFoundException e) {
-			
-	        sendMessage(message.getChat().getId(), "Non riesco a trovare il magnifico regolamento del clan. @asdamp sistema subito!");
+		if (regolamento == null) {
+			try {
+				regolamento = readAll(new FileInputStream("Regolamento.txt"));
+			}
 
+			catch (FileNotFoundException e) {
+
+				sendMessage(message.getChat().getId(),
+						"Non riesco a trovare il magnifico regolamento del clan. @asdamp sistema subito!");
+				return;
+			}
 		}
-    	
+        sendMessage(message.getChat().getId(), regolamento, new OptionalArgs().disableWebPagePreview());
+
     }
-    
+	private static String readAll(InputStream input) {
+		Scanner scanner = new Scanner(input);
+		scanner.useDelimiter("\\A");
+		return scanner.hasNext() ? scanner.next() : null;
+	}
+	
     @MessageHandler(contentTypes = Message.Type.TEXT)
     public void handleTextMessage(Message message) {
         log.info(String.format("%s: %s", message.getChat().getId(), message.getText()));
-        /*replyTo(message, message.getText());*/
+        replyTo(message, message.getText());
     }
 
     @DefaultHandler
